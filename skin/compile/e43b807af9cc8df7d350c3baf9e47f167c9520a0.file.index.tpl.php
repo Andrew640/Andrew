6a892acf1238f33c14d239f5bd7f537d3e63a5ac
Andrew640 <andrew.thomas640@gmail.com>
2014-03-28 16:55:34 +0000

    Added smarty

---
+++ skin/compile/e43b807af9cc8df7d350c3baf9e47f167c9520a0.file.index.tpl.php
@@ -0,0 +1,37 @@
+<?php /* Smarty version Smarty-3.1.16, created on 2014-03-28 16:52:37
+         compiled from "index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:286183695335a8d5dcfde5-82238411%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'e43b807af9cc8df7d350c3baf9e47f167c9520a0' => 
+    array (
+      0 => 'index.tpl',
+      1 => 1396024991,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '286183695335a8d5dcfde5-82238411',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.16',
+  'unifunc' => 'content_5335a8d5dd7d14_07796207',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5335a8d5dd7d14_07796207')) {function content_5335a8d5dd7d14_07796207($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+	<head>
+	
+		<link rel="stylesheet" href="date.css" />
+		
+    <title>Start</title>
+    
+    </head>
+  	
+	<body>
+
+		<div class="date">Hello</div>
+
+	</body>
+</html><?php }} ?>
